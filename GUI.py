0f0ecde0d700ea060694732e2bf76c38de9a73be
Xecus <abo.elfotouh@live.com>
2019-05-26 13:28:28 +0200

    split the main frame into subframes

--- GUI.py
+++ GUI.py
@@ -3,7 +3,7 @@ from tkinter import filedialog
 import pandas as pd
 import handler
 
-class Application(Frame):
+class TopFrame(Frame):
 
     def AxisSelectiong(self,data):
         pass
@@ -16,9 +16,6 @@ class Application(Frame):
             self.FileLabel['text'] = file.name
             print(data)
 
-    def StatusBar(self):
-        pass
-
     def BrowseWidgets(self):
         self.BrowseLabel = Label(self,
                                 text = 'Choose your CSV file: ')
@@ -27,15 +24,19 @@ class Application(Frame):
                             command = self.OpenFile)
         self.FileLabel = Label(self)
 
-        self.BrowseLabel.grid(row = 0, column = 0, columnspan = 2)
-        self.Browse.grid(row = 0, column = 2)
-        self.FileLabel.grid(row = 1, column = 0,columnspan = 3)
+        # self.BrowseLabel.grid(row = 0, column = 0, columnspan = 2)
+        # self.Browse.grid(row = 0, column = 2)
+        # self.FileLabel.grid(row = 1, column = 0,columnspan = 3)
+        self.BrowseLabel.pack(side = LEFT)
+        self.Browse.pack(side = LEFT)
+        self.FileLabel.pack()
 
     def GenerateWidget(self):
         self.Generate = Button(self,
                                 text = 'Generate HTML')
 
-        self.Generate.grid(row = 2, column = 0, columnspan = 2)
+        # self.Generate.grid(row = 2, column = 0, columnspan = 2)
+        self.Generate.pack()
 
     def QuitWidget(self):
         self.QUIT = Button(self,
@@ -43,7 +44,8 @@ class Application(Frame):
                             fg = 'red',
                             command = self.quit)
 
-        self.QUIT.grid(row = 2, column = 2, columnspan = 2)
+        # self.QUIT.grid(row = 2, column = 2, columnspan = 2)
+        self.QUIT.pack()
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -52,7 +54,23 @@ class Application(Frame):
         self.GenerateWidget()
         self.QuitWidget()
 
+class StatusFrame(Frame):
+
+    def StatusBar(self):
+        self.status = Label(self,
+                        text = 'Help information .. ',
+                        bd = 1,
+                        relief = SUNKEN,
+                        anchor = NW)
+        self.status.pack(side = LEFT, fill = X)
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack(side = BOTTOM)
+        self.StatusBar()
+
 root = Tk()
-app = Application(master=root)
-app.mainloop()
+top = TopFrame(master=root)
+status = StatusFrame(master=root)
+root.mainloop()
 root.destroy()
