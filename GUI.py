b134dad08016f399beb7c6dcd0da26262a0ccc93
Xecus <abo.elfotouh@live.com>
2019-05-26 11:36:47 +0200

    initiate GUI script and change gitignore file

--- GUI.py
+++ GUI.py
@@ -1,1 +1,52 @@
 from tkinter import *
+from tkinter import filedialog
+import pandas as pd
+import handler
+
+class Application(Frame):
+
+    def AxisSelectiong(self,data):
+        pass
+
+    def OpenFile(self):
+        file = filedialog.askopenfile(parent=root,mode='rb',title='Choose a file',filetypes = [('CSV Files','*.csv')])
+        if file:
+            data = pd.read_csv(file)
+            file.close()
+            print(data)
+            self.FileLabel['text'] = file.name
+
+    def StatusBar(self):
+        pass
+
+    def BrowseWidgets(self):
+        self.BrowseLabel = Label(self,
+                                text = 'Choose your CSV file: ' )
+        self.Browse = Button(self,
+                            text = "Browse ...",
+                            command = self.OpenFile)
+
+        self.BrowseLabel.pack(side = LEFT)
+        self.Browse.pack(side = LEFT)
+
+        self.FileLabel = Label(self)
+        self.FileLabel.pack()
+
+    def QuitWidget(self):
+        self.QUIT = Button(self,
+                            text = 'Quit',
+                            fg = 'red',
+                            command = self.quit)
+
+        self.QUIT.pack(side = BOTTOM)
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.BrowseWidgets()
+        self.QuitWidget()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GUI-Data-Presenter
 
 Create html file for csv data imported with the selected graph
+
+by running the GUI.py script
